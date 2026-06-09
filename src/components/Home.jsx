4d33ee2cab3cc17588621b433b161ec8f00ccf64
sineenath1704 <sineenathrueangsri@gmail.com>
2026-06-09 14:40:14 +0700

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <section id="home">
+      <h1>สวัสดี ฉันคือ ...</h1>
+    </section>
+  )
+}
+
+export default Home  
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar() {
+  return (
+    <nav>
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+      <a href="#projects">Projects</a>
+      <a href="#contact">Contact</a>
+    </nav>
+  )
+}
+
+export default Navbar  // 
