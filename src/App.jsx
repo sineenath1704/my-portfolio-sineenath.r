29cf3b5e32247f4fe19117c485598a6f38f96f3a
sineenath1704 <sineenathrueangsri@gmail.com>
2026-09-09 23:16:53 +0700

    Update Delete Landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import { useEffect } from 'react'
-import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+} from 'react-router-dom'
+
 import { LanguageProvider } from './context/LanguageContext'
-import Landing from './components/Landing'
+
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About'
@@ -9,6 +16,7 @@ import Projects from './components/Projects'
 import Contact from './components/Contact'
 import BackToTop from './components/BackToTop'
 import ResumeSwitcher from './components/Resume_Switcher'
+
 import ProjectDetailTemplate from './components/ProjectForJSX/ProjectDetailTemplate'
 import ProjectDetailONEPUT from './components/ProjectForJSX/Intern/ProjectDetailONEPUT'
 import ProjectDetailFocusRoom from './components/ProjectForJSX/University/ProjectDetailFocusRoom'
@@ -16,18 +24,22 @@ import ProjectDetailCPAxtraMile from './components/ProjectForJSX/Hack/ProjectDet
 import ProjectDetailHerEvidence from './components/ProjectForJSX/Hack/ProjectDetailHerEvidence'
 import ProjectDetailSITHelloWorldHippo from './components/ProjectForJSX/Hack/ProjectDetailSITHelloWorldHippo'
 
+
 function ScrollToHash() {
   const { pathname, hash, search } = useLocation()
 
   useEffect(() => {
     if (hash) {
       const id = hash.replace('#', '')
+
       const scrollToSection = () => {
         const element = document.getElementById(id)
+
         if (element) {
           element.scrollIntoView({ behavior: 'smooth' })
           return true
         }
+
         return false
       }
 
@@ -35,6 +47,7 @@ function ScrollToHash() {
         const t1 = setTimeout(scrollToSection, 100)
         const t2 = setTimeout(scrollToSection, 300)
         const t3 = setTimeout(scrollToSection, 600)
+
         return () => {
           clearTimeout(t1)
           clearTimeout(t2)
@@ -42,12 +55,16 @@ function ScrollToHash() {
         }
       }
     } else {
-      window.scrollTo({ top: 0, behavior: 'smooth' })
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      })
     }
   }, [pathname, hash, search])
 
   return null
 }
+
 
 function MainLayout() {
   return (
@@ -62,34 +79,120 @@ function MainLayout() {
   )
 }
 
+
 function App() {
   return (
     <LanguageProvider>
       <BrowserRouter>
         <ScrollToHash />
-        <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/home" element={<MainLayout />} />
-        <Route path="/projects" element={<Navigate to="/home#projects" replace />} />
-        <Route path="/Projects" element={<Navigate to="/home#projects" replace />} />
-        <Route path="/Projects.jsx" element={<Navigate to="/home#projects" replace />} />
-        <Route path="/Project.jsx" element={<Navigate to="/home#projects" replace />} />
-        <Route path="/resume" element={<ResumeSwitcher />} />
-        <Route path="/Resume_Switcher" element={<ResumeSwitcher />} />
-        <Route path="/Resume_Switcher.jsx" element={<ResumeSwitcher />} />
 
-        {/* Project Detail Routes */}
-        <Route path="/project-detail" element={<ProjectDetailTemplate />} />
-        <Route path="/project-detail/oneput" element={<ProjectDetailONEPUT />} />
-        <Route path="/ProjectDetailONEPUT" element={<ProjectDetailONEPUT />} />
-        <Route path="/project-detail/focus-room" element={<ProjectDetailFocusRoom />} />
-        <Route path="/ProjectDetailFocusRoom" element={<ProjectDetailFocusRoom />} />
-        <Route path="/project-detail/cp-axtra-mile" element={<ProjectDetailCPAxtraMile />} />
-        <Route path="/ProjectDetailCPAxtraMile" element={<ProjectDetailCPAxtraMile />} />
-        <Route path="/project-detail/herevidence" element={<ProjectDetailHerEvidence />} />
-        <Route path="/ProjectDetailHerEvidence" element={<ProjectDetailHerEvidence />} />
-        <Route path="/project-detail/sit-hello-world-hippo" element={<ProjectDetailSITHelloWorldHippo />} />
-        <Route path="/ProjectDetailSITHelloWorldHippo" element={<ProjectDetailSITHelloWorldHippo />} />
+        <Routes>
+
+          {/* Main Home */}
+          <Route path="/home" element={<MainLayout />} />
+
+          {/* ถ้าเข้า "/" ให้ไป Home */}
+          <Route
+            path="/"
+            element={<Navigate to="/home" replace />}
+          />
+
+          {/* Projects */}
+          <Route
+            path="/projects"
+            element={<Navigate to="/home#projects" replace />}
+          />
+
+          <Route
+            path="/Projects"
+            element={<Navigate to="/home#projects" replace />}
+          />
+
+          <Route
+            path="/Projects.jsx"
+            element={<Navigate to="/home#projects" replace />}
+          />
+
+          <Route
+            path="/Project.jsx"
+            element={<Navigate to="/home#projects" replace />}
+          />
+
+
+          {/* Resume */}
+          <Route
+            path="/resume"
+            element={<ResumeSwitcher />}
+          />
+
+          <Route
+            path="/Resume_Switcher"
+            element={<ResumeSwitcher />}
+          />
+
+          <Route
+            path="/Resume_Switcher.jsx"
+            element={<ResumeSwitcher />}
+          />
+
+
+          {/* Project Detail Routes */}
+
+          <Route
+            path="/project-detail"
+            element={<ProjectDetailTemplate />}
+          />
+
+          <Route
+            path="/project-detail/oneput"
+            element={<ProjectDetailONEPUT />}
+          />
+
+          <Route
+            path="/ProjectDetailONEPUT"
+            element={<ProjectDetailONEPUT />}
+          />
+
+          <Route
+            path="/project-detail/focus-room"
+            element={<ProjectDetailFocusRoom />}
+          />
+
+          <Route
+            path="/ProjectDetailFocusRoom"
+            element={<ProjectDetailFocusRoom />}
+          />
+
+          <Route
+            path="/project-detail/cp-axtra-mile"
+            element={<ProjectDetailCPAxtraMile />}
+          />
+
+          <Route
+            path="/ProjectDetailCPAxtraMile"
+            element={<ProjectDetailCPAxtraMile />}
+          />
+
+          <Route
+            path="/project-detail/herevidence"
+            element={<ProjectDetailHerEvidence />}
+          />
+
+          <Route
+            path="/ProjectDetailHerEvidence"
+            element={<ProjectDetailHerEvidence />}
+          />
+
+          <Route
+            path="/project-detail/sit-hello-world-hippo"
+            element={<ProjectDetailSITHelloWorldHippo />}
+          />
+
+          <Route
+            path="/ProjectDetailSITHelloWorldHippo"
+            element={<ProjectDetailSITHelloWorldHippo />}
+          />
+
         </Routes>
       </BrowserRouter>
     </LanguageProvider>
